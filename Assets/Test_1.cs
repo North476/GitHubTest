f12111686caa5732e8ae97ee593c5438a28ca14d
王奕666 <2847910447@qq.com>
2026-05-22 19:37:18 +0800

    添加预设体
    添加脚本，预设体
    
    Co-Authored-By: North476 <204129838+North476@users.noreply.github.com>

--- Assets/Test_1.cs
+++ Assets/Test_1.cs
@@ -1,14 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.Diagnostics;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test_1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("sdsad");
+        
     }
 
     // Update is called once per frame
