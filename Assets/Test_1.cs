dc32dac77eb36a8ab716ae31face7bbd1d5f4bc7
North476 <jwshsa@163.com>
2026-05-24 16:12:58 +0800

    11

--- Assets/Test_1.cs
+++ Assets/Test_1.cs
@@ -7,7 +7,7 @@ public class Test_1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello");
     }
 
     // Update is called once per frame
